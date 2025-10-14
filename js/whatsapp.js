db9052510c225235f073e7e458dc6704d1ec12a3
Samdeveloper2000 <sameershaikh2320.ss@gmail.com>
2025-10-14 13:42:41 +0530

    add whatapp features

---
+++ js/whatsapp.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const whatsappButton = document.querySelector(".whatsapp-icon");
+    function handleScroll() {
+        if (window.scrollY > 50) {
+            whatsappButton.classList.add("visible");
+        } else {
+            whatsappButton.classList.remove("visible");
+        }
+    }
+    window.addEventListener("scroll", handleScroll);
+});
