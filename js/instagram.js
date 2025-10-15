53f186a72e59cfa4d5a633ca9f8fd95405fce674
Samdeveloper2000 <sameershaikh2320.ss@gmail.com>
2025-10-16 00:54:50 +0530

    text changed and logo  change

--- js/instagram.js
+++ js/instagram.js
@@ -1,47 +1,59 @@
-'use client';
+// 'use client';
 
-import React, { useEffect } from 'react';
-import Link from 'next/link';
+// import React, { useEffect } from 'react';
+// import Link from 'next/link';
 
-function Instagram() {
-    useEffect(() => {
-        const InstagramButton = document.querySelector('.Instagram-icon');
+// function Instagram() {
+//     useEffect(() => {
+//         const InstagramButton = document.querySelector('.Instagram-icon');
 
-        const handleScroll = () => {
-            if (window.scrollY > 50) {
-                InstagramButton.classList.add('visible');
-            } else {
-                InstagramButton.classList.remove('visible');
-            }
-        };
+//         const handleScroll = () => {
+//             if (window.scrollY > 50) {
+//                 InstagramButton.classList.add('visible');
+//             } else {
+//                 InstagramButton.classList.remove('visible');
+//             }
+//         };
 
-        window.addEventListener('scroll', handleScroll);
+//         window.addEventListener('scroll', handleScroll);
 
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-    }, []);
+//         return () => {
+//             window.removeEventListener('scroll', handleScroll);
+//         };
+//     }, []);
 
-    return (
-        <>
-            {/* Instagram ICON AREA START */}
-            <div className="Instagram-icon">
-                <Link
-                    href="https://www.instagram.com/raahconstraofficial?igsh=eWs5djBkMXVvYW96_source=ig_contact_invite"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="flex items-center justify-center"
-                >
-                    <img
-                        src="/assets/images/logo/icons8-instagram.png"
-                        alt="Instagram"
-                        className="Instagram-icon-img"
-                    />
-                </Link>
-            </div>
-            {/* Instagram ICON AREA END */}
-        </>
-    );
-}
+//     return (
+//         <>
+//             {/* Instagram ICON AREA START */}
+//             <div className="Instagram-icon">
+//                 <Link
+//                     href="https://www.instagram.com/raahconstraofficial?igsh=eWs5djBkMXVvYW96_source=ig_contact_invite"
+//                     target="_blank"
+//                     rel="noopener noreferrer"
+//                    // className="flex items-center justify-center"
+//                 >
+//                     <img
+//                         src="/assets/images/logo/icons8-instagram.png"
+//                         alt="Instagram"
+//                         className="Instagram-icon-img"
+//                     />
+//                 </Link>
+//             </div>
+//             {/* Instagram ICON AREA END */}
+//         </>
+//     );
+// }
 
-export default Instagram;
+// export default Instagram;
+
+document.addEventListener("DOMContentLoaded", function () {
+    const whatsappButton = document.querySelector("instagram-icon");
+    function handleScroll() {
+        if (window.scrollY > 50) {
+            whatsappButton.classList.add("visible");
+        } else {
+            whatsappButton.classList.remove("visible");
+        }
+    }
+    window.addEventListener("scroll", handleScroll);
+});
